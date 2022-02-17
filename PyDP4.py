28452e227b3bba2098fe850623fd5de951f050b2
HowarthA <ah809@cam.ac.uk>
2022-02-17 14:20:27 +0000

    nfp

--- PyDP4.py
+++ PyDP4.py
@@ -539,9 +539,11 @@ def main(settings):
 
             DP5data.Output = DP5.MakeOutput( Isomers, settings,DP5data,DP5data.DP5_Error_probs,DP5data.B_ErrorAtomProbs)
 
-            #f = open("/".join(str(settings.OutputFolder).split("/")[:-1]) + "/abs_mean_max_bayes", "a+")
+            f = open("/".join(str(settings.OutputFolder).split("/")[:-1]) + "/abs_mean_max_bayes", "a+")
 
-            #f.write( str(DP5data.CMAE) + " "  +str(DP5data.CMax)  + "\n")
+            f.write( str(DP5data.CMAE) + " "  +str(DP5data.CMax)  + "\n")
+
+            quit()
 
         else:
 
