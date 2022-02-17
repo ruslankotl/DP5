6306cdcecc2ea4e8c9dea2d14ed6ed50b69fd4ab
HowarthA <ah809@cam.ac.uk>
2022-02-17 12:09:09 +0000

    nfp

--- DP5.py
+++ DP5.py
@@ -400,13 +400,13 @@ def Rescale_DP5(Mol_probs,BAtomProbs,Settings,DP5type,CMAE):
 
         for iso in range(0, len(Mol_probs)):
 
-            #DP5AtomProbs[iso] = [ Exp_scaling_function(x) for x in BAtomProbs[iso]]
+            DP5AtomProbs[iso] = [ Exp_scaling_function(x) for x in BAtomProbs[iso]]
 
-            #DP5probs.append(float( Exp_scaling_function(Mol_probs[iso]) ))
+            DP5probs.append(float( Exp_scaling_function(Mol_probs[iso]) ))
 
-            DP5AtomProbs[iso] = [ x for x in BAtomProbs[iso]]
+            #DP5AtomProbs[iso] = [ x for x in BAtomProbs[iso]]
 
-            DP5probs.append(float( Mol_probs[iso]) )
+            #DP5probs.append(float( Mol_probs[iso]) )
 
             #DP5AtomProbs[iso] = [float(correct_kde.pdf(x) / (incorrect_kde.pdf(x) + correct_kde.pdf(x))) for x in
              #                    BAtomProbs[iso]]
