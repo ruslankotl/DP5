63ad61083c4504d34b88e74ca58b87df31aa85c6
ruslankotl <rk582@cam.ac.uk>
2024-07-29 23:10:25 +0100

    Prettier logging of shifts

--- dp5/nmr_processing/description_files.py
+++ dp5/nmr_processing/description_files.py
@@ -18,7 +18,9 @@ def process_description(nmr_source):
                 equivalents.append(line[:-1].split(","))
             elif "OMIT" in line:
                 omits.extend(line[5:-1].split(","))
+    logger.info("Read carbon NMR shifts")
     C_labels, C_exp = _parse_description(Cexp)
+    logger.info("Read proton NMR shifts")
     H_labels, H_exp = _parse_description(Hexp)
 
     return C_labels, C_exp, H_labels, H_exp, equivalents, omits
@@ -31,15 +33,12 @@ def _parse_description(exp):
         texp = re.sub(r"or|OR", ",", exp, flags=re.DOTALL)
         texp = re.sub(r" ", "", texp, flags=re.DOTALL)
         # Get all assignments, split mulitassignments
-        expLabels = re.findall(r"(?<=\().*?(?=\)|;)",
-                               texp, flags=re.DOTALL)
+        expLabels = re.findall(r"(?<=\().*?(?=\)|;)", texp, flags=re.DOTALL)
         expLabels = [x.replace("any", "") for x in expLabels]
         expLabels = [x.split(",") for x in expLabels]
         # Remove assignments and get shifts
-        ShiftData = (re.sub(r"\(.*?\)", "", exp.strip(), flags=re.DOTALL)).split(
-            ","
-        )
-        logger.info(ShiftData)
+        ShiftData = (re.sub(r"\(.*?\)", "", exp.strip(), flags=re.DOTALL)).split(",")
+        logger.info(", ".join(ShiftData))
         expShifts = [float(x) for x in ShiftData]
     else:
         expLabels = []
