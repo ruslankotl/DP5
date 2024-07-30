d558e7d6b2256926d3761656eac3fe73ad9d7b44
ruslankotl <rk582@cam.ac.uk>
2024-07-30 16:08:19 +0100

    remove dead code

--- dp5/nmr_processing/__init__.py
+++ dp5/nmr_processing/__init__.py
@@ -1,7 +1,5 @@
 from dp5.nmr_processing.Carbon_processing import process_carbon
-from dp5.nmr_processing.Proton_processing import process_proton
 from dp5.nmr_processing.Carbon_assignment import AssignCarbon
-from dp5.nmr_processing.Proton_assignment import AssignProton
 from dp5.nmr_processing.Carbon_plotting import PlotCarbon
 from dp5.nmr_processing.Proton_plotting import PlotProton
 from dp5.nmr_processing.nmr_ai import NMRData
