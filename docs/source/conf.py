28dd2e9b1ab173f073bfd2e9092b6136553b608a
ruslankotl <rk582@cam.ac.uk>
2024-07-31 13:11:42 +0100

    add dp5 to system path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,6 @@
+import sys
+
+sys.path.append("../../dp5")
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
